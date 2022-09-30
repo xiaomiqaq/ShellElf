dfbe5c6388ff7352732260f0e6a1d2c630c11f5f
zpl <xiaomiqaq@163.com>
2022-09-30 07:18:17 -0700

    add jump struction

--- packer64/ELFImage.cpp
+++ packer64/ELFImage.cpp
@@ -111,71 +111,53 @@ Elf64_Phdr* ELFImage::GetProgramHeader(Elf64_Word type, Elf64_Word flag)
 
 bool ELFImage::DeleteUseless()
 {
-	if(!m_fileBuffer) return false;
-	size_t newBuffSize=0;
-	int phentNum = FileHeader()->e_phnum;
-	Elf64_Phdr lastPh = ProgramHeader()[phentNum-1];
-	newBuffSize = lastPh.p_offset+lastPh.p_filesz;
-	
-	if (FileHeader()->e_shoff >= newBuffSize) 
-	{
-		FileHeader()->e_shoff = 0;
-		FileHeader()->e_shnum = 0;
-		FileHeader()->e_shstrndx = 0;
-	}
 
-	Byte *newElfBuf = (Byte*)malloc(newBuffSize);
-	memcpy(newElfBuf, m_fileBuffer, newBuffSize);
-	free(m_fileBuffer);
-	m_fileBuffer = newElfBuf;
-	m_fileBuffer.size = newBuffSize;
-	return true;
 }
 
 bool ELFImage::GenerateElfFile(const char* filename)
 {
-	ofstream outputFile(filename,ofstream::out|ofstream::binary);
+	// ofstream outputFile(filename,ofstream::out|ofstream::binary);
 
-	Elf64_Addr entry_vaddr = sizeof(Elf64_Ehdr) + sizeof(Elf32_Phdr);
-	Elf64_Ehdr ehdr;
-	ehdr.e_ident[EI_MAG0] = ELFMAG0;
-	ehdr.e_ident[EI_MAG1] = ELFMAG1;
-	ehdr.e_ident[EI_MAG2] = ELFMAG2;
-	ehdr.e_ident[EI_MAG3] = ELFMAG3;
-	ehdr.e_ident[EI_CLASS] = ELFCLASS64;
-	ehdr.e_ident[EI_DATA] = ELFDATA2LSB;
-	ehdr.e_ident[EI_VERSION] = EV_CURRENT;
-	ehdr.e_ident[EI_OSABI] = ELFOSABI_SYSV;
- 	ehdr.e_ident[EI_ABIVERSION] = 0;
-	memset(ehdr.e_ident + EI_PAD, 0, EI_NIDENT - EI_PAD);
-	ehdr.e_type = ET_EXEC;
-	ehdr.e_machine = EM_X86_64;
-	ehdr.e_version = EV_CURRENT;
-	ehdr.e_entry = entry_vaddr;
-	ehdr.e_phoff = sizeof(Elf64_Ehdr);
-	ehdr.e_shoff = 0;
-	ehdr.e_flags = 0;
-	ehdr.e_ehsize = sizeof(Elf64_Ehdr);
-	ehdr.e_phentsize = sizeof(Elf64_Phdr);
-	ehdr.e_phnum = 2;
-	ehdr.e_shentsize = sizeof(Elf64_Shdr);
-	ehdr.e_shnum = 0;
-	ehdr.e_shstrndx = SHN_UNDEF;
-	outputFile.write((char*)&ehdr,sizeof(ehdr));
+	// Elf64_Addr entry_vaddr = sizeof(Elf64_Ehdr) + sizeof(Elf32_Phdr);
+	// Elf64_Ehdr ehdr;
+	// ehdr.e_ident[EI_MAG0] = ELFMAG0;
+	// ehdr.e_ident[EI_MAG1] = ELFMAG1;
+	// ehdr.e_ident[EI_MAG2] = ELFMAG2;
+	// ehdr.e_ident[EI_MAG3] = ELFMAG3;
+	// ehdr.e_ident[EI_CLASS] = ELFCLASS64;
+	// ehdr.e_ident[EI_DATA] = ELFDATA2LSB;
+	// ehdr.e_ident[EI_VERSION] = EV_CURRENT;
+	// ehdr.e_ident[EI_OSABI] = ELFOSABI_SYSV;
+ 	// ehdr.e_ident[EI_ABIVERSION] = 0;
+	// memset(ehdr.e_ident + EI_PAD, 0, EI_NIDENT - EI_PAD);
+	// ehdr.e_type = ET_EXEC;
+	// ehdr.e_machine = EM_X86_64;
+	// ehdr.e_version = EV_CURRENT;
+	// ehdr.e_entry = entry_vaddr;
+	// ehdr.e_phoff = sizeof(Elf64_Ehdr);
+	// ehdr.e_shoff = 0;
+	// ehdr.e_flags = 0;
+	// ehdr.e_ehsize = sizeof(Elf64_Ehdr);
+	// ehdr.e_phentsize = sizeof(Elf64_Phdr);
+	// ehdr.e_phnum = 2;
+	// ehdr.e_shentsize = sizeof(Elf64_Shdr);
+	// ehdr.e_shnum = 0;
+	// ehdr.e_shstrndx = SHN_UNDEF;
+	// outputFile.write((char*)&ehdr,sizeof(ehdr));
 
-	int app_offset = sizeof(Elf64_Ehdr) + sizeof(Elf64_Phdr) ;
-	Elf64_Phdr app_phdr;
-	app_phdr.p_type = PT_LOAD;
-	app_phdr.p_offset = app_offset;
-	app_phdr.p_vaddr = PACKED_BIN_ADDR + app_offset; /* Keep vaddr aligned */
-	app_phdr.p_paddr = app_phdr.p_vaddr;
-	app_phdr.p_filesz = m_fileBuffer.size;
-	app_phdr.p_memsz = m_fileBuffer.size;
-	app_phdr.p_flags = PF_R | PF_W;
-	app_phdr.p_align =  0x200000;
-	outputFile.write((char*)&app_phdr,sizeof(app_phdr));
+	// int app_offset = sizeof(Elf64_Ehdr) + sizeof(Elf64_Phdr) ;
+	// Elf64_Phdr app_phdr;
+	// app_phdr.p_type = PT_LOAD;
+	// app_phdr.p_offset = app_offset;
+	// app_phdr.p_vaddr = PACKED_BIN_ADDR + app_offset; /* Keep vaddr aligned */
+	// app_phdr.p_paddr = app_phdr.p_vaddr;
+	// app_phdr.p_filesz = m_fileBuffer.size;
+	// app_phdr.p_memsz = m_fileBuffer.size;
+	// app_phdr.p_flags = PF_R | PF_W;
+	// app_phdr.p_align =  0x200000;
+	// outputFile.write((char*)&app_phdr,sizeof(app_phdr));
 
-	outputFile.write((char*)m_fileBuffer,m_fileBuffer.size);
+	// outputFile.write((char*)m_fileBuffer,m_fileBuffer.size);
 
 	return true;
 }
@@ -184,25 +166,25 @@ void ELFImage::InsertShell(Byte* shellBuf)
 {
 	//make shell
 	//modi eh
-	FileHeader()->e_shnum = FileHeader()->e_shnum + 1;
-	//m_fileBuffer.size = m_fileBuffer.size + FileHeader()->e_shentsize + shellBuf.siz;
-	Elf64_Phdr* ph = GetProgramHeader(PT_LOAD,PF_R|PF_X);
+	// FileHeader()->e_shnum = FileHeader()->e_shnum + 1;
+	// //m_fileBuffer.size = m_fileBuffer.size + FileHeader()->e_shentsize + shellBuf.siz;
+	// Elf64_Phdr* ph = GetProgramHeader(PT_LOAD,PF_R|PF_X);
 
-	Elf64_Shdr* textSec = GetSectHeaderByName(".text");
-	size_t textSecHeaderOff = (size_t)textSec-(size_t)m_fileBuffer;
-	Elf64_Shdr shellSH;
-	PushToShtr(".shell");
-	shellSH.sh_name = GetShStrIndex(".shell");
-	shellSH.sh_addr = SHN_UNDEF;
-	shellSH.sh_type = SHT_PROGBITS;
-	shellSH.sh_flags = SHF_EXECINSTR;
-	shellSH.sh_addr =0;
-	shellSH.sh_offset = 0;
-	shellSH.sh_link =0;
-	shellSH.sh_info=0;
-	shellSH.sh_addralign = 1;
-	shellSH.sh_entsize = sizeof(shellSH);
-	m_fileBuffer.Insert({(Byte*)&shellSH,sizeof(shellSH)},m_fileBuffer.size);
+	// Elf64_Shdr* textSec = GetSectHeaderByName(".text");
+	// size_t textSecHeaderOff = (size_t)textSec-(size_t)m_fileBuffer;
+	// Elf64_Shdr shellSH;
+	// PushToShtr(".shell");
+	// shellSH.sh_name = GetShStrIndex(".shell");
+	// shellSH.sh_addr = SHN_UNDEF;
+	// shellSH.sh_type = SHT_PROGBITS;
+	// shellSH.sh_flags = SHF_EXECINSTR;
+	// shellSH.sh_addr =0;
+	// shellSH.sh_offset = 0;
+	// shellSH.sh_link =0;
+	// shellSH.sh_info=0;
+	// shellSH.sh_addralign = 1;
+	// shellSH.sh_entsize = sizeof(shellSH);
+	// m_fileBuffer.Insert({(Byte*)&shellSH,sizeof(shellSH)},m_fileBuffer.size);
 
 	// Elf64_Off shellOff = textSec->sh_offset + textSec->sh_size;
 	// Byte* newBuf = (Byte*)malloc(m_fileBuffer.size);
@@ -227,33 +209,12 @@ void ELFImage::AddShell(ELFImage shell)
 void ELFImage::BuildFile(const char* name)
 {
 	ofstream ofs(name,ofstream::out|ofstream::binary);
-	ofs.write((char*)m_fileBuffer,m_fileBuffer.size);
-	//size_t totalSize = m_eh.e_shoff + sizeof(Elf64_Shdr)*m_secTable.size();
-	//char* newFileBuf = (char*)malloc(totalSize);
+	
+	//get file size
+	int fileSize = 0;
+	fileSize+= (FileHeader()->e_shoff +FileHeader()->e_shentsize*FileHeader()->e_shnum);
 
-	// ofs.write((char*)&m_eh,sizeof(m_eh));
-	// for(auto &ph:m_phTable)
-	// {
-	// 	ofs.write((char*)&ph,sizeof(ph));
-	// }
-	// char* zeroBuf = new char[40960];
-	// memset(zeroBuf,0,1024);
-	// for(auto &sec:m_secTable)
-	// {
-	// 	if(sec.header.sh_type == SHT_NOBITS) continue;
-	// 	size_t pos = ofs.tellp();
-
-	// 	if(pos < sec.header.sh_offset)
-	// 	{
-	// 		size_t padding = sec.header.sh_offset-pos;
-	// 		ofs.write(zeroBuf,padding);
-	// 	}
-	// 	ofs.write((char*)sec.data,sec.header.sh_size);
-	// }
-	// for(auto &sec:m_secTable)
-	// {
-	// 	ofs.write((char*)&sec,sizeof(sec));
-	// }
+	ofs.write((char*)m_fileBuffer,fileSize);
 	
 }
 
@@ -279,13 +240,13 @@ std::string ELFImage::GetShStr(Elf64_Word index)
 void ELFImage::PushToShtr(std::string str)
 {
 	Elf64_Shdr* header = GetSectHeaderByName(".shstrtab");
-	size_t addPos = header->sh_offset+header->sh_size;
-	size_t newSize = str.size() + 1;
+	// size_t addPos = header->sh_offset+header->sh_size;
+	// size_t newSize = str.size() + 1;
 
 	
-	m_fileBuffer.Insert({(Byte*)str.c_str(),newSize},addPos);
-	FileHeader()->e_shoff = FileHeader()->e_shoff + newSize;
-	GetSectHeaderByName(".shstrtab")->sh_size+= newSize;
+	// m_fileBuffer.Insert({(Byte*)str.c_str(),newSize},addPos);
+	// FileHeader()->e_shoff = FileHeader()->e_shoff + newSize;
+	// GetSectHeaderByName(".shstrtab")->sh_size+= newSize;
 }
 
 Elf64_Word ELFImage::GetShStrIndex(std::string str)
--- packer64/main.cpp
+++ packer64/main.cpp
@@ -1,27 +1,61 @@
 #include <iostream>
 #include "ELFImage.h"
 #include <cstring>
+typedef Elf64_Addr Addr ;
+
 const char* fileName = "../Tests/hello_a64";
-
-
-
-void GetTextEnd(ELFImage &elf)
+const char* outFileName = "../Tests/hello_a64_";
+typedef struct _b_instruction
 {
-    Elf64_Shdr* textSh = elf.GetSectHeaderByName(".text");
-    Elf64_Addr textEnd = (Elf64_Addr)elf.m_fileBuffer + textSh->sh_offset + textSh->sh_size;
-}
+	Elf64_Word offset : 24;			//偏移
+	Elf64_Word op : 8;			//重定位方式)
+}JmpInstruction;
 
+Elf64_Addr GetTextEndOff(ELFImage &elf)
+{
+    /* process space addr of .text's last byte  */
+    Elf64_Shdr* textSh = elf.GetSectHeaderByName(".text");
+    Elf64_Addr textEnd = textSh->sh_addr + textSh->sh_size;
+    return textEnd;
+}
+void AddJmpInstruction(ELFImage &elf)
+{
+    /*  1. calculate b offset : offset = (entry - text_end)/4
+        2. add b to text_end(file space), add .text section size
+        3. check .text.off+size < (.text+1).off
+        4. change entry*/
+    Elf64_Addr textEnd = GetTextEndOff(elf);
+    Elf64_Addr entry = elf.FileHeader()->e_entry;
+    JmpInstruction b;
+    b.offset = (entry - textEnd)/4;
+    b.op = 0x17;
+
+    Elf64_Shdr* textSh = elf.GetSectHeaderByName(".text");
+     JmpInstruction* pB = (JmpInstruction*) 
+                        ( (Addr)elf.m_fileBuffer + textSh->sh_offset +  textSh->sh_size);
+    *pB = b;
+    textSh->sh_size += 4;
+
+    elf.FileHeader()->e_entry = textEnd;
+
+}
 
 int main(int argc, char** argv)  
 {
+    if (argc < 2)
+    {
+        printf("input file path!");
+        return 0;
+    }
+
+    char fileName[50] = {0};
+    char outFileName[50] = {0};
+    strncpy(fileName,argv[1],strlen(argv[1]));
+    strncpy(outFileName,argv[1],strlen(argv[1]));
+    strncat(outFileName,"_",1);
+
     ELFImage elf(fileName);
-    Elf64_Shdr* sh = elf.GetSectHeaderByName(".text");
-    elf.PushToShtr(".shell");
-    Elf64_Word a=elf.GetShStrIndex(".shell");
-    std::string s = elf.GetShStr(a);
-    Elf64_Phdr* ph = elf.GetProgramHeader(PT_LOAD,PF_R|PF_X);
-    ELFImage shell("../stub/shell.o");
-    elf.AddShell(shell);
-    elf.BuildFile("x86_pack");
-    std::cout<<"start sh"<<std::endl;
+    AddJmpInstruction(elf);
+    elf.BuildFile(outFileName);
+    std::cout<<"success!"<<std::endl;
 }

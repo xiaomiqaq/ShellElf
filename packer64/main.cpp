9a5f6051e822f4af31b7af7fcf5d2ca0e8a5f3d0
zpl <xiaomiqaq@163.com>
2022-10-20 21:11:27 -0700

    11

--- packer64/main.cpp
+++ packer64/main.cpp
@@ -5,23 +5,23 @@
 
 typedef Elf64_Addr Addr ;
 
-const char* fileName = "../Tests/hello_a64";
-const char* outFileName = "../Tests/hello_a64_";
-const char* shellName = "../stub/shell.o";
+//const char* fileName = "../Tests/hello_a64";
+//const char* outFileName = "../Tests/hello_a64_";
+//const char* shellName = "../stub/shell.o";
 
 int main(int argc, char** argv)  
 {
-    // if (argc < 2)
-    // {
-    //     printf("input file path!");
-    //     return 0;
-    // }
+    if (argc < 2)
+     {
+         printf("input file path!");
+         return 0;
+     }
 
-    // char fileName[50] = {0};
-    // char outFileName[50] = {0};
-    // strncpy(fileName,argv[1],strlen(argv[1]));
-    // strncpy(outFileName,argv[1],strlen(argv[1]));
-    // strncat(outFileName,"_",1);
+     char fileName[50] = {0};
+     char outFileName[50] = {0};
+     strncpy(fileName,argv[1],strlen(argv[1]));
+     strncpy(outFileName,argv[1],strlen(argv[1]));
+     strncat(outFileName,"_",1);
     ELFImage elf(fileName);
     ELFImage shell(shellName);
     Packer packer(elf);
